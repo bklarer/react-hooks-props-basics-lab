826c5b015966c3916afaa005151d05b0548658f1
Benjamin Klarer <benjaminklarer@gmail.com>
2022-07-25 20:01:09 -0700

    commit tree 0eb68a15927dedb34d4de48145b4282bbd97cd75

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({bio, link}) {
+  
+  console.log(bio)
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && bio.length < 1 ?  null : <p>{bio}</p> }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links link={link}/>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Links({link}) {
+    
+    return(
+    <>
+        <h3>Links</h3>
+        <a href={link.github}>{link.github}</a>
+        <a href={link.linkedin}>{link.linkedin}</a>
+    </>
+
+    );
+}
+
+
+export default Links;
